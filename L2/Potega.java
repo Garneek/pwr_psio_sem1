db1b814a83804386a0fd243d6c25da27cbed2285
Garneek <garneek255@gmail.com>
2024-10-22 21:41:56 +0200

    Added BigInt and BigDec factorial and exp. Testing upper bounds of calculation

--- L2/Potega.java
+++ L2/Potega.java
@@ -1,10 +1,12 @@
+import java.math.BigDecimal;
+
 class Potega{
  /**
   * Returns x to the power of k 
   * 
   * @param x base - real number
   * @param k exponent - natural number
-  * @returns x to the power of b
+  * @return x to the power of k
   */
   public static double power(double x, int k){
     if (k == 0) return 1f;
@@ -15,6 +17,22 @@ class Potega{
     return x * power(x, k - 1);
   }
 
+ /**
+  * Returns x to the power of k both x and k can be big numbers
+  * 
+  * @param x base - real number 
+  * @param k exponent - natural number
+  * @return x to the power of k
+  * */
+  public static BigDecimal powerBD(BigDecimal x, int k){
+	if (k == 0) return BigDecimal.valueOf(1f);
+	if (k % 2 == 0) {
+	  BigDecimal temp = powerBD(x, k / 2);
+	  return temp.multiply(temp);
+	} 
+    return x.multiply(powerBD(x, k - 1));
+  }
+  
   public static void main(String[] args){
     System.out.printf("Obliczanie potegi z x i k:\n");
     System.out.printf("> %f^%d = %f\n", 2f, 0, power(2f, 0));
@@ -22,7 +40,20 @@ class Potega{
     System.out.printf("> %f^%d = %f\n", 2f, 1, power(2f, 1));
     System.out.printf("> %f^%d = %f\n", 3f, 2, power(3f, 2));
     System.out.printf("> %f^%d = %f\n", 2.5f, 2, power(2.5f, 2));
+    int i = 0;
+    while(power(10f, i) != Double.POSITIVE_INFINITY) {
+    	i++;
+    }
+    System.out.printf("Maksymalna potega 10 dla double: 10^%d\n", i - 1);
+    System.out.println("Typ danych o duzo wiekszym zakresie dla liczb rzeczywistych to BigDecimal");
+    System.out.println("Obliczanie potegi z x i k za pomoca BigDecimal:");
+    System.out.printf("> %f^%d = %f\n", 2f, 0, powerBD(BigDecimal.valueOf(2f), 0));
+    System.out.printf("> %f^%d = %f\n", 2f, 10, powerBD(BigDecimal.valueOf(2), 10));
+    System.out.printf("> %f^%d = %f\n", 2f, 1, powerBD(BigDecimal.valueOf(2), 1));
+    System.out.printf("> %f^%d = %f\n", 3f, 2, powerBD(BigDecimal.valueOf(3), 2));
+    System.out.printf("> %f^%d = %f\n", 2.5f, 2, powerBD(BigDecimal.valueOf(2.5), 2));
+    System.out.printf("> %f^%d = %f\n", 2f, 500, powerBD(BigDecimal.valueOf(2), 500));
   }
-  
+ 
 }
 
--- L2/Silnia.java
+++ L2/Silnia.java
@@ -1,6 +1,8 @@
+import java.math.BigInteger;
+
 class Silnia{
  /**
-  * Returns factorial of a natural number x 
+  * Returns factorial of a natural number x
   * 
   * @param x factorial argument
   * @returns factorial of x
@@ -12,6 +14,20 @@ class Silnia{
     }
     return ans;
   }
+  
+  /**
+   * Returns factorial of a big natural number x
+   * 
+   * @param x factorial argument
+   * @returns factorial of x
+   */
+   public static BigInteger factorialBI(int x){
+     BigInteger ans = BigInteger.ONE;
+     for (int i = 1; i <= x; i++){
+       ans = ans.multiply(BigInteger.valueOf(i));
+     }
+     return ans;
+   }
 
   public static void main(String[] args){
     System.out.printf("Obliczanie silni z n:\n");
@@ -19,6 +35,17 @@ class Silnia{
     System.out.printf("> %d! = %d\n", 1, factorial(1));
     System.out.printf("> %d! = %d\n", 2, factorial(2));
     System.out.printf("> %d! = %d\n", 5, factorial(5));
+    int i = 0;
+    while (factorial(i) > 0) {
+      i++;
+    }
+    System.out.printf("Maksymalna wartosc dla int: %d\n", i - 1);
+    System.out.println("Typ danych o duzo wiekszym zakresie dla liczb calkowity to BigInteger");
+    System.out.printf("Obliczanie silni z n za pomoca BigInteger:\n");
+    System.out.printf("> %d! = %d\n", 0, factorialBI(0));
+    System.out.printf("> %d! = %d\n", 1, factorialBI(1));
+    System.out.printf("> %d! = %d\n", 5, factorialBI(5));
+    System.out.printf("> %d! = %d\n", 50, factorialBI(50));
   }
   
 }
